ec9283b69f9e5fe75971e06182851f386576c482
isacoper <belacoper@gmail.com>
2018-10-09 18:22:24 -0300

    gerenciador com alterações parciais do css

---
+++ test/system/tarefas_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class TarefasTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit tarefas_url
+  #
+  #   assert_selector "h1", text: "Tarefa"
+  # end
+end
